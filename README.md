4b85c8e0ab50148cd5cca9bdad4a9fb52702577f
Dominick Triola <dominicktriola@gmail.com>
2022-04-18 17:30:15 -0700

    Initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dom's UI Components
---
+++ lib/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface IButtonProps extends React.DetailedHTMLProps<React.
+  ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {};
+
+export const Button: React.FunctionComponent<IButtonProps> = props => {
+  const { children } = props;
+
+  return <button>{children}</button>;
+};
